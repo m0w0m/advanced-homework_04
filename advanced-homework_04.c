0bfb251cc0cb88f40d42ddc0af076ecff034f00e
m0w0m <48341730+m0w0m@users.noreply.github.com>
2019-05-31 22:26:40 +0800

    Add files via upload

---
+++ advanced-homework_04.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <ctype.h> 
+#include <string.h>
+int main(void)
+{
+	char slogan1[]="Taiwan,Touch,Your,Heart";
+	char slogan2[50];
+	int i,j=0,length;
+	
+	length=strlen(slogan1);
+	
+	for(i=0;i<length;i++)
+	{
+		if(isalpha(slogan1[i])!=0||isspace(slogan1[i])!=0)
+		{
+			slogan2[j]=slogan1[i];
+			j++;
+		}
+	}
+	slogan2[j]='\0';
+	puts(slogan2);
+	system("pause");
+	return 0;
+}
